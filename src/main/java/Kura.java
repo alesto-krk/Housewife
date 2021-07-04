e6eb3a5fc5a997e1e5a12dbfd2eea9f60a9d00f3
alesto-krk <aleksandrajaworska87@gmail.com>
2021-07-04 15:38:35 +0200

    Nowa aplikacja z uzyciem mavena
    strona tytulowa i przejscie na nastepna strone

---
+++ src/main/java/Kura.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class Kura extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("tytul.fxml"));
+        primaryStage.setTitle("Kura Domowa");
+        Image icon = new Image(getClass().getResourceAsStream("kura-image.png"));
+        primaryStage.getIcons().add(icon);
+        primaryStage.setScene(new Scene(root, 355, 400));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
