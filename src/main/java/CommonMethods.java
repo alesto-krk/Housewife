c8e4c35e38c39f54d7e24be8053256f07ed40406
alesto-krk <aleksandrajaworska87@gmail.com>
2021-11-08 13:48:48 +0100

    Jest datePicker i dziala, jest szkielet ListyZadan

---
+++ src/main/java/CommonMethods.java
@@ -0,0 +1,7 @@
+public class CommonMethods {
+
+    //showAlert
+    //backToMenu
+    //backTo(String np lista zadan na dzis)
+    //checkIfFileExists ?
+}
--- src/main/java/ListaZadanNaDzisController.java
+++ src/main/java/ListaZadanNaDzisController.java
@@ -1,14 +1,21 @@
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 
-public class ListaZadanNaDzisController {
+public class ListaZadanNaDzisController {       //jak wyskoczy lista zadan do wykonania opcja done i zostaw na potem + zapisz
 
     @FXML
-    Label date = new Label();
+    Label date;
+    @FXML
+    Label chosenDate;
+    @FXML
+    DatePicker datePicker;
 
     LocalDate todaysDate = LocalDate.now();
 
@@ -16,7 +23,22 @@ public class ListaZadanNaDzisController {
         //date.setText(todaysDate.toString());
        String df =  DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL).format(todaysDate).toString();
        date.setText(df);
+    }
 
+    public void chosenDatePicker(ActionEvent event){
+        if(!datePicker.getValue().isBefore(todaysDate)) {
+            LocalDate dateForLabel = datePicker.getValue();
+            String dfl = DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL).format(dateForLabel).toString();
+            chosenDate.setText(dfl);
+        }
+        else {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Warning");
+            alert.setHeaderText("Nie da się ustawić zadań na minione dni :)");
+            alert.setContentText(null);
+            alert.show();
+        }
+       //String a = String.valueOf(dateForLabel);
     }
 
 
